3677ee19f1d97e3ef4f3259b4cc24e0a4c4ccb54
Daniel Felizardo <daniel.david772@gmail.com>
2019-07-20 11:12:55 -0300

    Persist data for users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
@@ -25,10 +26,25 @@ export default class Main extends Component {
     loading: false,
   };
 
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
   handleAddUser = async () => {
     const { users, newUser } = this.state;
 
-    this.setState({ loadong: true });
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
