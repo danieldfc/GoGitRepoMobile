0b9b35d99956e0fba545a034e7a71f0b1253b5a9
Daniel Felizardo <daniel.david772@gmail.com>
2019-07-20 07:54:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Rocketseat</Text>
+    </View>
+  );
+}
